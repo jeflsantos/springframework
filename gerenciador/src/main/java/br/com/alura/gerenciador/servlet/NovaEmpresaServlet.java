197ca17d40345d1b83e75397066dd41ee46a60c2
Jefferson Luis <jeflsantos@hotmail.com>
2022-06-23 15:19:23 -0300

    Java Servlet: programação web Java Java Servlet: programação web Java
    Listando empresas com servlets

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -15,10 +15,16 @@ public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
+		
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
 		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com Sucesso!</body></html>");

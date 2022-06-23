8df6b7bd7f614bfcd5e826457792c961b6c0ade0
Jefferson Luis <jeflsantos@hotmail.com>
2022-06-23 09:05:24 -0300

    Java Servlet: programação web Java Java Servlet: programação web Java
    Finalmente, o primeiro servlet.

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi mundo, parabens voce escreveu o primeiro Servlets.");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O Servlet OiMundoServlet foi chamado.");
+	}
+}

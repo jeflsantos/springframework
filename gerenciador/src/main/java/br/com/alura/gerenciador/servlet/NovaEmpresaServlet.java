8bf8ac86b6f572ff2a7659aa21d2a760017e42bb
Jefferson Luis <jeflsantos@hotmail.com>
2022-06-23 11:54:50 -0300

    Java Servlet: programação web Java Java Servlet: programação web Java
    Metodos GET e POST

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -17,7 +17,9 @@ public class NovaEmpresaServlet extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
+		
 		String nomeEmpresa = request.getParameter("nome");
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com Sucesso!</body></html>");
 		

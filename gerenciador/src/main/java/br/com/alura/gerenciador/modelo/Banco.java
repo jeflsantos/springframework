9652c9e5e8821deac961ed8d1ad21ce8e6fe07aa
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-04 20:39:44 -0300

    Java Servlet: autenticação, autorização e o padrão MVC Java Servlet: autenticação, autorização e o padrão MVC
    Preparando modelo e view

--- gerenciador/src/main/java/br/com/alura/gerenciador/modelo/Banco.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static  Integer chaveSequencial = 1;
 	
 	static {
@@ -19,6 +20,20 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		lista.add(empresa);
 		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("nico");
+		u1.setSenha("12345");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("jeff");
+		u2.setSenha("12345");
+		
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
+		
+		
 	}
 
 	public void adiciona(Empresa empresa) {

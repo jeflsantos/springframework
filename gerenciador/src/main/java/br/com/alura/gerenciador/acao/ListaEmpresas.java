c43807da73d2ef771b333fcfef3b47cb6da0730d
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-04 21:57:32 -0300

    Java Servlet: autenticação, autorização e o padrão MVC Java Servlet: autenticação, autorização e o padrão MVC
    Testando o login

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -13,6 +14,11 @@ import br.com.alura.gerenciador.modelo.Empresa;
 public class ListaEmpresas implements Acao{
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession sessao = request.getSession();
+		if (sessao.getAttribute("usuarioLogado") == null) {
+			return "redirect:entrada?acao=LoginForm";
+		}
 		
 		System.out.println("Listando Empresas");
 		

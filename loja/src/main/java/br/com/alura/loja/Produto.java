1a069815721208af9e7bfd84380113517b974560
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-06 15:02:34 -0300

    Maven: gerenciamento de dependências e build de aplicações Java Maven: gerenciamento de dependências e build de aplicações Java
    Estrutura de um projeto com Maven

---
+++ loja/src/main/java/br/com/alura/loja/Produto.java
@@ -0,0 +1,10 @@
+package br.com.alura.loja;
+
+public class Produto {
+
+	/* Estrutura de um projeto Maven:
+	 * 	
+	 * 		src/main/java: Deverá conter apenas aplicações classe java;
+	 * 
+	 */
+}
---
+++ loja/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProdutoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+		/*
+		 * src/test/java: Aqui fica codigos de testes;
+		 */
+	}
+
+}

76d73064e44ceddedb0b677da390a93dcc845f8a
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-06 06:58:25 -0300

    Java Servlet: autenticação, autorização e o padrão MVC Java Servlet: autenticação, autorização e o padrão MVC
    Serviço com XML

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/EmpresasService.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/EmpresasService.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -22,11 +22,21 @@ public class EmpresasService extends HttpServlet {
 		
 		List<Empresa> empresas = new Banco().getEmpresas();
 		
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
 		
-		response.setContentType("application/json");
-		response.getWriter().print(json);
+		XStream xstream = new XStream();
+		xstream.alias("empresa", Empresa.class);
+		String xml = xstream.toXML(empresas);
+		
+		response.setContentType("application/xml");
+		response.getWriter().print(xml);
+		
+		
+		
+//		Gson gson = new Gson();
+//		String json = gson.toJson(empresas);
+//		
+//		response.setContentType("application/json");
+//		response.getWriter().print(json);
 		
 		
 	}

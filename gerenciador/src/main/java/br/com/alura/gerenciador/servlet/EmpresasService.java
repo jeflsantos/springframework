6432ef6b3bee00b8d3dd37542db93f2eb8e0fa68
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-06 07:33:39 -0300

    Java Servlet: autenticação, autorização e o padrão MVC Java Servlet: autenticação, autorização e o padrão MVC
    Cliente do serviço

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/EmpresasService.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/EmpresasService.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.gerenciador.modelo.Banco;
@@ -22,22 +23,28 @@ public class EmpresasService extends HttpServlet {
 		
 		List<Empresa> empresas = new Banco().getEmpresas();
 		
+		String valor = request.getHeader("accept");
 		
-		XStream xstream = new XStream();
-		xstream.alias("empresa", Empresa.class);
-		String xml = xstream.toXML(empresas);
-		
-		response.setContentType("application/xml");
-		response.getWriter().print(xml);
-		
-		
-		
-//		Gson gson = new Gson();
-//		String json = gson.toJson(empresas);
-//		
-//		response.setContentType("application/json");
-//		response.getWriter().print(json);
-		
+		if (valor.contains("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
+			
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+			
+		} else if (valor.endsWith("json")) {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+			
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message':'no content'}");
+		}
+			
 		
 	}
 
